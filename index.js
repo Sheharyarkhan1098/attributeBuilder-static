0f867f331bf370689b037d6438c433e94eb2e721
sheharyar <sheharyar.khan1098@gmail.com>
2021-10-20 13:27:09 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,32 @@
+
+$('#my_select_box').change(function () {
+    $('#my_changing_image').attr('src', $('#my_select_box').val());
+});
+
+$('#my_select_box1').change(function () {
+$('#my_changing_image1').attr('src', $('#my_select_box1').val());
+});
+
+$('#my_select_box2').change(function () {
+$('#my_changing_image2').attr('src', $('#my_select_box2').val());
+});
+
+$('#my_select_box3').change(function () {
+$('#my_changing_image3').attr('src', $('#my_select_box3').val());
+});
+
+$('#my_select_box4').change(function () {
+$('#my_changing_image4').attr('src', $('#my_select_box4').val());
+});
+
+$('#my_select_box5').change(function () {
+$('#my_changing_image5').attr('src', $('#my_select_box5').val());
+});
+
+$('#my_select_box6').change(function () {
+$('#my_changing_image6').attr('src', $('#my_select_box6').val());
+});
+$('#my_select_box7').change(function () {
+$('#my_changing_image7').attr('src', $('#my_select_box7').val());
+});
+
